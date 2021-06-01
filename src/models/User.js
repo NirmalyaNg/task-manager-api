d723cce0a17921338f4269c77e5a210ce229132e
Nirmalya Ganguly <nirmalyaganguly1999@gmail.com>
2021-06-01 20:42:45 +0530

    Slight changes

--- src/models/User.js
+++ src/models/User.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
       default: 0,
       validate(value) {
         if (value < 0) {
-          throw new Error("Age should be positive");
+          throw new Error("Age must be positive");
         }
       },
     },
